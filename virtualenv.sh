7dfbdff86b46bf279347adf1cd14a19e79854910
oz volmar <o.volmar@gmail.com>
2022-07-30 17:01:04 -0400

    updated virtualenv.sh

--- virtualenv.sh
+++ virtualenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #echo "********  To invoke this script use:  source $0 *********"
-VENV="~/.capstone"
+VENV="$HOME/.capstone"
 echo ${VENV}
 #test -d ${VENV} || mkdir -v ${VENV}
 python3 -m venv ${VENV}
