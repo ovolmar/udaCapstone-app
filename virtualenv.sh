95bacf7c6752b7e5aadb0be21dd0c9ca6e0b9bd4
oz volmar <o.volmar@gmail.com>
2022-07-30 17:47:06 -0400

    integrating eksctl

--- virtualenv.sh
+++ virtualenv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#echo "********  To invoke this script use:  source $0 *********"
+echo "********  To invoke this script use:  source $0 *********"
 VENV="$HOME/.capstone"
 echo ${VENV}
-#test -d ${VENV} || mkdir -v ${VENV}
+test -d ${VENV} || mkdir -v ${VENV}
 python3 -m venv ${VENV}
 source ${VENV}/bin/activate
