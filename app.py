95ff4919d91fa2717693545d82823aca9bba7bda
oz volmar <o.volmar@gmail.com>
2022-08-14 16:09:57 -0400

    changed app port to listen on 8080

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def delete_city( name ):
     return redirect(url_for('index_get'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
