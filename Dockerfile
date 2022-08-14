17e6951a5e1f6e582e6d213ca421243b428d7993
oz volmar <o.volmar@gmail.com>
2022-08-14 16:11:02 -0400

    last commit of Sunday

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ WORKDIR /app
 COPY . /app/
 
 Run pip install -r requirements.txt 
-
+EXPOSE 8080
 ENTRYPOINT ["python"]
 
 CMD ["app.py"]
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,6 +8,6 @@ kubectl set image $IMAGE_NAME
 kubectl get pods 
 
 mypod=$(kubectl get pods  | grep $IMAGE_NAME  | awk '{print $1}')
-kubectl port-forward $mypod  8000:80
+kubectl port-forward $mypod  $LOCAL_PORT:$CONTAINER_PORT
 
 #
