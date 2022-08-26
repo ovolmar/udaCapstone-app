79b749a3624ee2b839ca6e2faba662345d8ff6a0
oz volmar <o.volmar@gmail.com>
2022-08-26 12:20:21 -0400

    test-1

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,6 @@
 
 
 source ./shell-vars.conf
-echo "TEST"
-# docker build -t $IMAGE_NAME .
-# docker image ls |grep $IMAGE_NAME
-# docker run -d -p $LOCAL_PORT:$CONTAINER_PORT $IMAGE_NAME
+docker build -t $IMAGE_NAME .
+docker image ls |grep $IMAGE_NAME
+docker run -d -p $LOCAL_PORT:$CONTAINER_PORT $IMAGE_NAME
