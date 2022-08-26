2687807961dab2f4646acd96badd966c8145cc8c
oz volmar <o.volmar@gmail.com>
2022-08-26 17:06:36 -0400

    added 5 sec timeout to app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ class City(db.Model) :
 def get_weather_data(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=b21a2633ddaac750a77524f91fe104e7"
     r = requests.get(url).json()
+    timeout=5
     return r
 
 @app.route('/')
