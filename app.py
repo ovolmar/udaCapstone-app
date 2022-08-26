41175ee42a44852a4577d065bfcc59b8e99136d9
oz volmar <o.volmar@gmail.com>
2022-08-26 17:09:15 -0400

    increased timeout to 30 sec timeout to app.py

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ class City(db.Model) :
 
 def get_weather_data(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=b21a2633ddaac750a77524f91fe104e7"
-    r = requests.get(url).json()
-    timeout=5
+    r = requests.get((url).json(), timeout=30)
     return r
 
 @app.route('/')
