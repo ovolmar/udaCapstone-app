4e5c4b1116eab3dc37b243f772d1bbe53f55f330
oz volmar <o.volmar@gmail.com>
2022-09-05 17:28:54 -0400

    adding a new tag

--- app.py
+++ app.py
@@ -7,10 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
-    return render_template('index.html', title="Deployment:---->", color="blue")
-    #return 'Project Capstone Developed by: Osvaldo Volmar!'
-
+    return render_template('index.html', title="Deployment Version:---->", color="BLUE 1.0")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
